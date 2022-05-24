7e7445a1de6e2783a2b7ab755d51cfa3d4f4ff8f
jignom368 <jingom369@gmail.com>
2022-05-24 23:09:32 +0900

    first commit

---
+++ Nature_project/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Seoul_forest(APIView):
+    def get(self, request):
+        return render(request, 'nature_project/seoul_forest.html')
