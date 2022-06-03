f70a03f3ca0220fe22271bc5d3f52223d65801d2
jignom368 <jingom369@gmail.com>
2022-06-04 01:46:10 +0900

    update

--- mainpage/models.py
+++ mainpage/models.py
@@ -6,6 +6,8 @@ class Apply(models.Model):
     phonenumber_1 = models.TextField()
     koreandance = models.TextField(null=True)
     PIagree= models.TextField(null=True)
+    category_fruit = (('푸릇푸릇한 자연을 좋아해서.','푸릇푸릇한 자연을 좋아해서.'),('푸릇푸릇한 자연을 좋아해서.','아무런 이유없이도 산책을 즐겨해요.'),('생각(고민)의 정리가 필요할 때.','생각(고민)의 정리가 필요할 때.'),('스트레스를 해소하기 위해서.','스트레스를 해소하기 위해서.'))
+    fruit = models.CharField(null=True, max_length=30, choices=category_fruit)
     category_Choices = (('푸릇푸릇한 자연을 좋아해서.','푸릇푸릇한 자연을 좋아해서.'),('푸릇푸릇한 자연을 좋아해서.','아무런 이유없이도 산책을 즐겨해요.'),('생각(고민)의 정리가 필요할 때.','생각(고민)의 정리가 필요할 때.'),('스트레스를 해소하기 위해서.','스트레스를 해소하기 위해서.'))
     how_think = models.CharField(null=True, max_length=30, choices=category_Choices)
     created_at = models.DateTimeField(auto_now_add=True)
