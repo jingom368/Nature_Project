876778da2844b7eef5c54362a5090b4c1ac48e55
jignom368 <jingom369@gmail.com>
2022-06-03 20:23:06 +0900

    first_check

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,13 +1,15 @@
+from unicodedata import category
 from django.db import models
 
 class Apply(models.Model):
     name = models.TextField()
-    phonenumber = models.TextField()
+    phonenumber_1 = models.TextField()
     koreandance = models.TextField(null=True)
     PIagree= models.TextField(null=True)
-    # how_think = models.TextField()
+    category_Choices = (('푸릇푸릇한 자연을 좋아해서.','푸릇푸릇한 자연을 좋아해서.'),('푸릇푸릇한 자연을 좋아해서.','아무런 이유없이도 산책을 즐겨해요.'),('생각(고민)의 정리가 필요할 때.','생각(고민)의 정리가 필요할 때.'),('스트레스를 해소하기 위해서.','스트레스를 해소하기 위해서.'))
+    how_think = models.CharField(null=True, max_length=30, choices=category_Choices)
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Email(models.Model):
-    email = models.TextField()
+class Contact(models.Model):
+    contact = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Apply, Email
+from .models import Apply, Contact
 from django.utils import timezone
 from django.http import HttpResponseRedirect
 
@@ -14,25 +14,25 @@ class Personal_Information(APIView):
         if request.method=='POST' and 'contact-submit-button' in request.POST:
             new_post = Apply()
             new_post.name = request.POST["name"]
-            new_post.phonenumber = request.POST["phonenumber"]
+            new_post.phonenumber_1 = request.POST["phonenumber_1"]
             new_post.koreandance = request.POST.getlist('answer_1[]')
             new_post.PIagree = request.POST.getlist('answer_2[]')
-            # new_post.how_think = request.POST.get('how-think')
+            new_post.how_think = request.POST.get('how-think')
             new_post.created_at = timezone.now()
             new_post.save()
 
         elif request.method=='POST' and 'footer-submit-button' in request.POST:
-            dancer_post = Email()
-            dancer_post.email = request.POST["email"]
+            dancer_post = Contact()
+            dancer_post.contact = request.POST["contact"]
             dancer_post.save()
 
         return HttpResponseRedirect("/")
 
 def seoul_forest_read(request):
     personal_list = Apply.objects.all().order_by("-pk")
-    dancer_email_list = Email.objects.all().order_by("-pk")
+    dancer_phonenumber_list = Contact.objects.all().order_by("-pk")
     context = {
         "personal_list": personal_list,
-        "dancer_email_list": dancer_email_list
+        "dancer_phonenumber_list": dancer_phonenumber_list
     }
     return render(request, "nature_project/seoul_forest_read.html", context)
